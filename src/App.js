ee44580edd033d87a8a36dbafadaac3e7bc384df
Selena Siri <selenasiri@gmail.com>
2021-02-10 14:43:05 -0500

    heading

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next'
+
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  function handleClick(lang) {
+    i18n.changeLanguage(lang);
+  }
+
   return (
     <div className="App">
+      <nav style={{ width: '100%', padding: '2rem 0', backgroundColor: 'gray' }}>
+        <button onClick={()=> handleClick('en')}>
+          English
+        </button>
+        <button onClick={()=> handleClick('ko')}>
+          Korean
+        </button>
+        <button onClick={()=> handleClick('chi')}>
+          Chinese
+        </button>
+      </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{t('my translated text')} </p>
+        
       </header>
     </div>
   );
